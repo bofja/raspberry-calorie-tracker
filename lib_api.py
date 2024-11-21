5f2654e6999d3436947363654a1d82081ceb9706
bofja <bof.ja1@gmail.com>
2024-11-21 11:12:54 -0500

    Update lib_api.py
    translated

--- lib_api.py
+++ lib_api.py
@@ -1,6 +1,6 @@
-# Digunakan saat mengirim dan menerima data ke FatSecret dan NodeRED
-# Masukan untuk FatSecret berupa jenis makanan yang diklasifikasi TFLite
-# TODO: Masukan untuk NodeRED berupa informasi gizi makanan dari FatSecret
+# Used when sending and receiving data to FatSecret and NodeRED
+# The input for FatSecret is a type of food classified by TFLite
+# TODO: Input to NodeRED is food nutritional information from FatSecret
 
 # FatSecret
 import requests
@@ -13,153 +13,153 @@ class FatSecret:
     def __init__(self, client_id, client_secret, token_file = None):
         self.client_id = client_id
         self.client_secret = client_secret
-        # Gunakan token dari file bila sudah ada
+        # Use the token from the file if it already exists
         if token_file: self.baca_token(token_file)
-        # Minta token baru bila token kosong dan simpan ke file
+        # Request a new token when the token is empty and save it to file
         if not self.token: self.autentikasi(token_file)
 
     def baca_token(self, file):
         try:
             with open(file, "r") as f:
-                # Simpan akses token dan hilangkan newline
+                # Keep the access token and remove the newline
                 self.token = f.readline().rstrip()
-            # Jangan pakai akses token bila sudah dihapus
+            # Keep the access token and remove the newline
             if self.token == "REMOVED": self.token = None
         except Exception:
-            print("Terjadi kesalahan saat mengakses file token...")
+            print("An error occurred while accessing the token file...")
             self.token = None
 
     def tulis_token(self, file, token):
         try:
             with open(file, "w") as f:
-                # Tulis token ke dalam file
+                # Write the token into a file
                 f.write(token)
             return True
         except Exception:
-            print("Terjadi kesalahan saat menulis file token...")
+            print("An error occurred while writing the token file...")
             return False
     
     def autentikasi(self, token_file = None):
-        # Cek dokumentasi pada https://platform.fatsecret.com/api/Default.aspx?screen=rapih
+        # Check the documentation on https://platform.fatsecret.com/api/Default.aspx?screen=rapih
         url = "https://oauth.fatsecret.com/connect/token"
         auth = rauth.HTTPBasicAuth(self.client_id, self.client_secret)
         data = { "grant_type": "client_credentials", "scope": "basic" }
-        # Minta akses token baru ke server FatSecret
+        # Request a new access token to the FatSecret server
         respons = requests.post(url = url, auth = auth, data = data)
         if respons.status_code == 200:
-            # Simpan akses token untuk digunakan nanti
+            # Save the access token for later use
             self.token = respons.json()["access_token"]
-            # Simpan akses token ke file bila diperlukan
+            # Save token access to files when needed
             if token_file: self.tulis_token(token_file, self.token)
         else:
-            # Cek kode error pada https://platform.fatsecret.com/api/Default.aspx?screen=rapiec
-            raise ConnectionError(f"Error {respons.status_code} saat meminta akses token...")
-
-    def cari_makanan(self, nama_makanan, jumlah_maks = 20, ambil_pertama = False):
+            # Check the error code on https://platform.fatsecret.com/api/Default.aspx?screen=rapiec
+            raise ConnectionError(f"Error {respons.status_code} when requesting token access...")
+     // cari_food      food_name max_count ambil_pertama
+    def search_food(self, food_name, max_count = 20, take_first = False):
         url = "https://platform.fatsecret.com/rest/server.api"
         headers = { "Authorization": f"Bearer {self.token}" }
-          # Lihat metode yang tersedia pada https://platform.fatsecret.com/api/Default.aspx?screen=rapiref2
-        data = { "method": "foods.search", "search_expression": nama_makanan, "format": "json" }
+          # See available methods on https://platform.fatsecret.com/api/Default.aspx?screen=rapiref2
+        data = { "method": "foods.search", "search_expression": food_name, "format": "json" }
         respons = requests.post(url = url, headers = headers, data = data)
         if respons.status_code == 200:
-            list_makanan = respons.json()["foods"]["food"]
-            # Cukup tampilkan X buah makanan bila diminta
-            list_makanan = list_makanan[:jumlah_maks] if jumlah_maks > 0 else list_makanan
-            if ambil_pertama == False:
-                # Pilih makanan berdasarkan hasil pencarian (menghasilkan id makanan)
-                for makanan in list_makanan:
-                    if makanan["food_type"] == "Brand": print(makanan["brand_name"], end = ": ")
-                    print(f'{makanan["food_name"]} [id: {makanan["food_id"]}]')
-                    print(f'{makanan["food_url"]}\n')
-                return int(input("Ketikkan id makanan yang dipilih: "))
+            list_food = respons.json()["foods"]["food"]
+            # Just display X items of food when prompted
+            list_food = list_food[:max_count] if max_count > 0 else list_food
+            if take_first == False:
+                # Select food based on search results (generate food id)
+                for food in list_food:
+                    if food["food_type"] == "Brand": print(food["brand_name"], end = ": ")
+                    print(f'{food["food_name"]} [id: {food["food_id"]}]')
+                    print(f'{food["food_url"]}\n')
+                return int(input("Type the selected food id: "))
             else:
-                # Ambil id makanan terkecil yang didapat dari list makanan
-                # Id makanan terkecil umumnya merupakan makanan asli (bukan brand tertentu)
-                return min([ int(makanan["food_id"]) for makanan in list_makanan ])
+                # Take the smallest food ID obtained from the food list
+                # The smallest food ID is generally real food (not a particular brand)
+                return min([ int(food["food_id"]) for food in list_food ])
         else:
-            # Cek kode error pada https://platform.fatsecret.com/api/Default.aspx?screen=rapiref2&method=foods.search
-            raise ConnectionError(f"Error {respons.status_code} saat memanggil foods.search...")
+            # Check the error code on https://platform.fatsecret.com/api/Default.aspx?screen=rapiref2&method=foods.search
+            raise ConnectionError(f"Error {respons.status_code} when calling foods.search...")
 
-    def info_makanan(self, id_makanan):
+    def info_food(self, id_food):
         url = "https://platform.fatsecret.com/rest/server.api"
         headers = { "Authorization": f"Bearer {self.token}" }
-        # Lihat metode yang tersedia pada https://platform.fatsecret.com/api/Default.aspx?screen=rapiref2
-        data = { "method": "food.get.v2", "food_id": id_makanan, "format": "json" }
+        # See available methods on https://platform.fatsecret.com/api/Default.aspx?screen=rapiref2
+        data = { "method": "food.get.v2", "food_id": id_food, "format": "json" }
         respons = requests.post(url = url, headers = headers, data = data)
         if respons.status_code == 200:
             return respons.json()["food"]
         else:
-            # Cek kode error pada https://platform.fatsecret.com/api/Default.aspx?screen=rapiref2&method=food.get.v2
-           raise ConnectionError(f"Error {respons.status_code} saat memanggil food.get.v2...")
+            # Check the error code on https://platform.fatsecret.com/api/Default.aspx?screen=rapiref2&method=food.get.v2
+           raise ConnectionError(f"Error {respons.status_code} when calling food.get.v2...")
     
-    # Dapatkan estimasi kalori per gram pada makanan
-    def estimasi_kalori(self, id_makanan):
-        info_makanan = self.info_makanan(id_makanan)
-        if info_makanan:
+    # Get an estimate of the calories per gram of food
+    def calorie_estimation(self, id_food):
+        info_food = self.info_food(id_food)
+        if info_food:
             kalori = []
-            # Dapatkan kalori untuk tiap-tiap takaran (ukuran) saji makanan
-            for takaran in info_makanan["servings"]["serving"]:
-                if takaran["metric_serving_unit"] == "g":
-                    # Dapatkan estimasi kalori per gram jika satuan berupa gram
-                    kalori.append(float(takaran["calories"]) / float(takaran["metric_serving_amount"]))
-            # Rata-ratakan kalori dari tiap-tiap takaran
+            # Get calories for each serving size (size) of food
+            for dose in info_food["servings"]["serving"]:
+                if dose["metric_serving_unit"] == "g":
+                    # Get an estimate of calories per gram if the units are grams
+                    kalori.append(float(dose["calories"]) / float(dose["metric_serving_amount"]))
+            # Average the calories from each serving
             kalori = round(sum(kalori) / len(kalori), 3)
             return int(kalori) if kalori.is_integer() else kalori
 
-# TODO: Baca statistik untuk NodeRED
+# TODO:Read the statistics for NodeRED
 class CsvHelper:
     def __init__(self, file_csv):
-        # File CSV untuk operasi baca tulis
+        # CSV file for read write operations
         self.file_csv = file_csv
 
-    def impor_csv(self, lewati_satu = True):
+    def impor_csv(self, skip_one = True):
         list_data = []
-        # Buka file CSV sebagai input
+        # Open a CSV file as input
         with open(self.file_csv, "r") as file:
-            # Lewati baris pertama (header) bila diminta
-            if lewati_satu: reader = list(csv.reader(file))[1:]
+            # Skip the first line (header) if requested
+            if skip_one: reader = list(csv.reader(file))[1:]
             else: reader = list(csv.reader(file))
             for bar in range(len(reader)):
-                # Baca kolom-kolm tiap baris
+                # Read the columns of each row
                 for kol in range(len(reader[bar])):
                     try:
                         if reader[bar][kol] in ("None", "NaN", ""):
-                            # Konversi string kosong menjadi null
+                            # Convert empty string to null
                             reader[bar][kol] = None
                         else:
-                            # Konversi string lainnya menjadi float atau integer
+                            # Convert other strings to float or integer
                             reader[bar][kol] = float(reader[bar][kol])
                             if reader[bar][kol].is_integer():
                                 reader[bar][kol] = int(reader[bar][kol])
                     except ValueError:
-                        # Jangan konversi string bila tidak memungkinkan
+                        # Do not convert strings if this is not possible
                         pass
-                # Tambahkan baris saat ini ke list data
+                # Add the current row to the data list
                 list_data.append(reader[bar])
-        # Kembalikan data dalam bentuk list 2 dimensi
+        # Return data in the form of a 2-dimensional list
         return list_data
 
     def ekspor_csv(self, list_data, label = True, on_linux = False):
-        # Hilangkan baris tambahan (\r\n) pada Windows
+        # Remove extra lines (\r\n) in Windows
         nl = "\n" if on_linux else ""
-        # Buka file CSV sebagai output (mode tulis)
+        # Open CSV file as output (write mode)
         with open(self.file_csv, "w", newline = nl) as file:
-            # Tulis nilai null (None) dan string kosong sebagai ""
+            # Write null values ​​(None) and empty strings as ""
             writer = csv.writer(file, delimiter = ",", quoting = csv.QUOTE_NONNUMERIC)
-            # Tambahkan label (header) bila diminta
-            if label: writer.writerow(["Waktu", "Nama", "Berat", "Kalori"])
-            # Tulis data ke CSV berdasarkan list data
+            # add a label (header) if requested
+            if label: writer.writerow(["Time", "Name", "Heavy", "Kalori"])
+            # Write data to CSV based on the data list
             writer.writerows(list_data)
 
-    # Tambah baris baru ke file csv
-    def tambah_csv(self, nama, berat, kalori_total, waktu = None, on_linux = False):
-        # Hilangkan baris tambahan (\r\n) pada Windows
+    # Add new row to csv file
+    def tambah_csv(self, name, heavy, kalori_total, time = None, on_linux = False):
+        # Remove extra lines (\r\n) in Windows
         nl = "\n" if on_linux else ""
-        # Buka file CSV sebagai output (mode tulis)
+        # Open CSV file as output (write mode)
         with open(self.file_csv, "a", newline = nl) as file:
-            # Tulis nilai null (None) dan string kosong sebagai ""
+            # Write null values ​​(None) and empty strings as ""
             writer = csv.writer(file, delimiter = ",", quoting = csv.QUOTE_NONNUMERIC)
-            # Dapatkan waktu saat ini bila waktu kosong
-            if not waktu: waktu = int(time.time())
-            # Tulis data ke CSV berdasarkan list data
-            writer.writerow([waktu, nama, berat, kalori_total])
+            # Get the current time if time is free
+            if not Time: Time = int(time.time())
+            # Write data to CSV based on the data list
+            writer.writerow([time, name, heavy, kalori_total])
